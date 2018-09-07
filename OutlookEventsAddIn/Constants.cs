885904a7a0ecefe0f3175bd3d6ca33e0c38f6584
sadiqmc <sadiqmc@gmail.com>
2018-09-07 11:15:31 +0000

    removed client id

--- OutlookEventsAddIn/Constants.cs
+++ OutlookEventsAddIn/Constants.cs
@@ -8,6 +8,6 @@ namespace OutlookEventsAddIn
 {
     class Constants
     {
-        public const string ClientId = "26bf5dc9-30c2-4ef9-89fa-4245f1d59b09"; // App name: VSTO-Events-AddIn
+        public const string ClientId = "APPLICATION_ID"; 
     }
 }
